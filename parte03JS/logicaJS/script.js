70191382248c91e9d4ccf9401b234725effa6e94
Arthur <arthurcaaldas@hotmail.com>
2023-10-15 16:29:43 -0300

    estudo

---
+++ parte03JS/logicaJS/script.js
@@ -0,0 +1,35 @@
+// var x = prompt("primeiro numero");
+// x = parseInt(x); 
+
+// var y = prompt("segundo numero");
+// y = parseInt(y); 
+
+// alert("A soma é: "  + (x + y));
+// alert("A subtração é: "  + (x - y));
+// alert("A multiplicação é: "  + (x * y));
+// alert("A divisão é: "  + (x / y));
+
+// --------------------------------------------------------------------------
+
+// var nota1 = prompt("primeira nota");
+// nota1 = parseFloat(nota1);
+
+// var nota2 = prompt("segunda nota");
+// nota2 = parseFloat(nota2);
+
+// var media = (nota1 + nota2) /2;
+
+// alert("A média das notas é: " + media);
+
+// ---------------------------------------------------------------------------
+
+// +  soma
+// -  subtração
+// *  multiplicação
+// /  divisão
+// %  modulo
+// ** exponenciação
+// ++ incremento
+// -- decremento
+
+//--------------------------------------------------------------------------------
